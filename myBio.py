6e9b6e6f042869fbdb57a5b996ded0d69782c8a0
ArthurGrimaud <arthur582strontium.gmail.com>
2018-12-08 23:48:45 +0100

    correction beug longestORF

--- myBio.py
+++ myBio.py
@@ -21,8 +21,6 @@ def getAllOrfCoor(sequenceDic,sequenceName): #retourne une liste de listes de co
 
 
 
-
-#
 # def display(S1coor,S2coor,S3coor,sequence,S4coor,S5coor,S6coor):
 #     allPosStrandCoor = [S1coor,S2coor,S3coor]
 #     allNegStrandCoor = [S4coor,S5coor,S6coor]
@@ -91,6 +89,8 @@ def getLengths(allCoor):
         for coor in frame:
             orfLength.append(coor[1]-coor[0])
             orfPos.append(coor)
+    print(orfLength)
+    print(orfPos)
     return (orfLength,orfPos)
 
 def getReadingFrame(allCoor,seqCoor):
@@ -103,8 +103,7 @@ def getReadingFrame(allCoor,seqCoor):
                     return int((frame-2)/-1)
 
 def getLongestORF(allCoor,sequence):
-    seqCoor = getLengths(allCoor)[1][getLengths(allCoor)[0].index(min(getLengths(allCoor)[0]))]
-
+    seqCoor = getLengths(allCoor)[1][getLengths(allCoor)[0].index(max(getLengths(allCoor)[0]))]
     sequenceDisplay = Tk()
 
     seqStart= Label(sequenceDisplay,text="Start Position :  "+str(seqCoor[0]))
@@ -250,50 +249,50 @@ def translate(seq,geneticTable):
 
     return aaSeq
 
-def enter_seq():
-    adn = input("Entrez␣la␣chaine␣:␣")
-    for i in range (len(adn) ):
-        print("i:␣", i, "->" , adn[i])
+# def enter_seq():
+#     adn = input("Entrez␣la␣chaine␣:␣")
+#     for i in range (len(adn) ):
+#         print("i:␣", i, "->" , adn[i])
 
-def Compte_aa(seq_prot):
-    """compte le nombre d'apparition d'un AA donné dans la séquence d'une prot"""
-    print("quel AA?")
-    aa = input()
-    nb_aa = 0
-    aa = aa.upper()
-    for i in seq_prot:
-        if i == aa :
-            nb_aa = nb_aa + 1
-    return nb_aa
-    print ("il␣y␣a␣" , Compte_aa(protein) , "␣Cysteine(s)")
+# def Compte_aa(seq_prot):
+#     """compte le nombre d'apparition d'un AA donné dans la séquence d'une prot"""
+#     print("quel AA?")
+#     aa = input()
+#     nb_aa = 0
+#     aa = aa.upper()
+#     for i in seq_prot:
+#         if i == aa :
+#             nb_aa = nb_aa + 1
+#     return nb_aa
+#     print ("il␣y␣a␣" , Compte_aa(protein) , "␣Cysteine(s)")
 
-def Compte_all_aa(seq_prot):
-    "affiche le nombre d'apparition de chaque AA dans une seq prot"
-    List_aa =["E","D","A","R","N","C","Q","G","H","I","L","K","M","F","P","S","T","W","Y","V"]
-    for i in List_aa :
-        Nb_aa = 0
-        for j in seq_prot:
-            if i == j:
-                Nb_aa = Nb_aa +1
-        print(i ,"->", Nb_aa)
+# def Compte_all_aa(seq_prot):
+#     "affiche le nombre d'apparition de chaque AA dans une seq prot"
+#     List_aa =["E","D","A","R","N","C","Q","G","H","I","L","K","M","F","P","S","T","W","Y","V"]
+#     for i in List_aa :
+#         Nb_aa = 0
+#         for j in seq_prot:
+#             if i == j:
+#                 Nb_aa = Nb_aa +1
+#         print(i ,"->", Nb_aa)
 
 
-def Is_dna(adn):
-    """retourne tru si la sequence entree est une sequence d adn """
-    seq = ""
-    for i in adn:
-        i = i.upper()
-        seq = seq + i
-    flag = 0
-    for i in seq :
-        if i=="A" or i=="T" or i=="G" or i=="C":
-            flag = flag
-        else :
-            flag = flag +1
-    if flag > 0 :
-        return False
-    else :
-        return True
+# def Is_dna(adn):
+#     """retourne tru si la sequence entree est une sequence d adn """
+#     seq = ""
+#     for i in adn:
+#         i = i.upper()
+#         seq = seq + i
+#     flag = 0
+#     for i in seq :
+#         if i=="A" or i=="T" or i=="G" or i=="C":
+#             flag = flag
+#         else :
+#             flag = flag +1
+#     if flag > 0 :
+#         return False
+#     else :
+#         return True
 
 def One_word(seq, start,wlen):
     """seq est une string entrée, start est l'index de départ du mot
@@ -312,19 +311,19 @@ def One_word(seq, start,wlen):
             return s
     return s
 
-def Count_word(seq, word):
-    """ seq est une string entrée, word est une string qui est recherchéen dans
-    la seq
-    Count_word retourne le nombre d'occurence de word dans la séquence et dans
-    tous les cadres de lecture"""
-    word = word.upper()
-    wlen = len(word)
-    flag = 0
-    for i in range(len(seq)):
-        Codon = One_word(seq,i,wlen)
-        if Codon == word :
-            flag = flag +1
-    print(word, "est présent", flag, "fois dans la séquence")
+# def Count_word(seq, word):
+#     """ seq est une string entrée, word est une string qui est recherchéen dans
+#     la seq
+#     Count_word retourne le nombre d'occurence de word dans la séquence et dans
+#     tous les cadres de lecture"""
+#     word = word.upper()
+#     wlen = len(word)
+#     flag = 0
+#     for i in range(len(seq)):
+#         Codon = One_word(seq,i,wlen)
+#         if Codon == word :
+#             flag = flag +1
+#     print(word, "est présent", flag, "fois dans la séquence")
 
 def Is_codon_start(seq,pos):
     seq = seq.upper()
@@ -373,7 +372,7 @@ def Anti_sens(dict_seq,seq_name):
 def Anti_sens_withSequence(seq):
     """retourne la seq d origine et la converti en brin anti sens, les sens de lecture es 5-3"""
     seq_anti = ""
-    temp_anti=""
+    temp_anti = ""
     temp_anti = seq[::-1]
     for n in temp_anti :
         if n == "A" :
